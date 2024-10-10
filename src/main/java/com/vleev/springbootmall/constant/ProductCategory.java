8a6972268c7c665f2f8c680c3323f5c77b27aee8
v-leev <vleev313@gmail.com>
2024-10-10 17:09:34 +0800

    refactor: 商品功能
    1.商品功能 - Refactor category，使用 Enum 類型

---
+++ src/main/java/com/vleev/springbootmall/constant/ProductCategory.java
@@ -0,0 +1,7 @@
+package com.vleev.springbootmall.constant;
+
+public enum ProductCategory {
+    FOOD,
+    CAR,
+    E_BOOK
+}
--- src/main/java/com/vleev/springbootmall/rowmapper/ProductRowMapper.java
+++ src/main/java/com/vleev/springbootmall/rowmapper/ProductRowMapper.java
@@ -1,5 +1,6 @@
 package com.vleev.springbootmall.rowmapper;
 
+import com.vleev.springbootmall.constant.ProductCategory;
 import com.vleev.springbootmall.model.Product;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -14,7 +15,7 @@ public class ProductRowMapper implements RowMapper<Product> {
 
         product.setProductId(rs.getInt("product_id"));
         product.setProductName(rs.getString("product_name"));
-        product.setCategory(rs.getString("category"));
+        product.setCategory(ProductCategory.valueOf(rs.getString("category")));
         product.setImageUrl(rs.getString("image_url"));
         product.setPrice(rs.getInt("price"));
         product.setStock(rs.getInt("stock"));
